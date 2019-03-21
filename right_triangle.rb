074940f2c8679eb2fdb1902d385fcea7dbb63f51
Anton Surganov <antonsurganov@yandex.ru>
2019-03-21 11:25:05 +0300

    Corrections 3

--- right_triangle.rb
+++ right_triangle.rb
@@ -3,17 +3,15 @@ a = gets.to_i
 b = gets.to_i
 c = gets.to_i
 
-if a == b && a == c
+cathetus1, cathetus2, hypotenuse = [a, b, c].sort!
+is_right = cathetus1**2 + cathetus2**2 == hypotenuse**2
+
+if cathetus1 == hypotenuse
   puts "Треугольник равнобедренный и равносторонний, но не прямоугольный"
+elsif is_right && cathetus1 == cathetus2
+  puts "Треугольник прямоугольный и равнобедренный"
+elsif is_right
+  puts "Треугольник прямоугольный"
 else
-  is_isosceles = a == b || a == c || b == c
-
-  cathetus1, cathetus2, hypotenuse = [a, b, c].sort!
-  is_right = cathetus1**2 + cathetus2**2 == hypotenuse**2
-
-  if is_right
-    puts "Треугольник прямоугольный#{" и равнобедренный" if is_isosceles}"
-  else
-    puts "Треугольник не прямоугольный"
-  end
+  puts "Треугольник не прямоугольный"
 end
